eebbe0542d9e6dc059807321c155881b3fcc7987
CoderRocha <guilhermerochas0420@gmail.com>
2024-08-26 20:32:15 -0300

    added to fetch 'collection' from 'firestore database'

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import { useFetch } from '../../hooks/useFetch'
+import React, { useEffect, useState } from 'react'
+import { useTheme } from '../../hooks/useTheme'
+import { projectFirestore } from '../../firebase/config'
 
 //styles
 import './Home.css'
@@ -8,10 +9,35 @@ import './Home.css'
 import RecipeList from '../../components/RecipeList'
 
 export default function Home() {
-  const { data, isPending, error } = useFetch("http://localhost:3000/recipes")
+  const { mode } = useTheme()
+  const [data, setData] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(false)
+  
+  useEffect(() => {
+    setIsPending(true)
+
+    projectFirestore.collection('recipes').get().then((snapshot) => {
+      if (snapshot.empty) {
+        setError('No Recipes to Load')
+        setIsPending(false)
+      } else {
+        let results = []
+        snapshot.docs.forEach(doc => {
+          results.push({ id: doc.id, ...doc.data() })
+        })
+        setData(results)
+        setIsPending(false)
+      }
+    }).catch(err => {
+      setError(err.message)
+      setIsPending(false)
+    })
+
+  }, [])
 
   return (
-    <div className="home">
+    <div className={`home ${mode}`}>
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
       {data && <RecipeList recipes={data} />}
