5f59f7de79ffa2bda16306c7d509ce31e430f33e
CoderRocha <guilhermerochas0420@gmail.com>
2024-08-26 20:55:07 -0300

    added option to add new recipes to firebase database

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef } from 'react'
 import { useState } from 'react'
-import { useFetch } from '../../hooks/useFetch'
 import { useNavigate } from 'react-router-dom'
 import { useTheme } from '../../hooks/useTheme'
+import { projectFirestore } from '../../firebase/config'
 
 // styles
 import './Create.css'
@@ -17,12 +17,17 @@ export default function Create() {
   const navigate = useNavigate()
   const { mode } = useTheme()
 
-  const { postData, data, error } = useFetch('http://localhost:3000/recipes', 'POST')
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    postData({ title, ingredients, method, cookingTime: cookingTime + ' minutes' })
+    const doc = ({ title, ingredients, method, cookingTime: cookingTime + ' minutes' })
+
+    try {
+    await projectFirestore.collection('recipes').add(doc)
+    navigate('/')
+  } catch(err) {
+    console.log(err)
   }
+}
 
   const handleAdd = (e) => {
     e.preventDefault()
@@ -35,12 +40,12 @@ export default function Create() {
     ingredientInput.current.focus()
   }
 
-  // redirect the user when we get the data response
-  useEffect(() => {
-    if (data) {
-      navigate('/')
-    }
-  }, [data, navigate]);
+  // // OLD JSON-SERVER REDIRECT SYSTEM TO redirect the user when we get the data response
+  // useEffect(() => {
+  //   if (data) {
+  //     navigate('/')
+  //   }
+  // }, [data, navigate]);
 
   return (
     <div className={`create ${mode}`}>
