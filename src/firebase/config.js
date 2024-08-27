0416717331a8874d77b11893d114c915cec93f11
Guilherme Rocha <108440515+CoderRocha@users.noreply.github.com>
2024-08-27 19:38:21 -0300

    Updated config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDo0CoOEN_ltidYabrNKLj1WngJlRf5BCc",
-    authDomain: "cooking-master-website.firebaseapp.com",
-    projectId: "cooking-master-website",
-    storageBucket: "cooking-master-website.appspot.com",
-    messagingSenderId: "960716251101",
-    appId: "1:960716251101:web:ce3cb12b40b0b6476b6034"
+    // Your Firebase Info Here
 }
 
 // initialize firebase
