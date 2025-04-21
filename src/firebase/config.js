dae25947201e6116c5a75fde7db2b17c805c35c7
Guilherme <guilhermerochas0420@gmail.com>
2025-04-20 22:20:58 -0300

    updated firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDo0CoOEN_ltidYabrNKLj1WngJlRf5BCc",
-    authDomain: "cooking-master-website.firebaseapp.com",
-    projectId: "cooking-master-website",
-    storageBucket: "cooking-master-website.appspot.com",
-    messagingSenderId: "960716251101",
-    appId: "1:960716251101:web:ce3cb12b40b0b6476b6034"
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId
 }
 
 // initialize firebase
